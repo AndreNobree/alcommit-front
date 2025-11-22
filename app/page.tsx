ca55c5af02e080297187a50b52822b66c6b24685
Infinitek <infinitek.corp@gmail.com>
2025-11-22 11:19:09 -0300

    criação de projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,39 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div className="bg-neutral-900 w-full h-full p-3 border-b border-green-500 flex items-center justify-between">
+        <div>
+          <h2 className="text-green-500 font-mono">AlCommit</h2>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex">
+          <button className="bg-green-500 text-neutral-900 font-mono px-3 py-1 rounded-md hover:bg-green-600 transition-colors mr-4 cursor-pointer">
+            <img src='/images/assets/terminal.png' alt="Terminal" className="inline-block w-5 h-5 mr-2" />
+            Terminal
+          </button>
+          <img src='/images/assets/configs.png' alt="Settings" className="w-5 h-5 cursor-pointer mt-1.5 mr-4" />
+          <img src='/images/assets/exit.png' alt="Exit" className="w-5 h-5 cursor-pointer mt-1.5" />
         </div>
-      </main>
+      </div>
+      <div className="w-full h-full flex justify-between">
+        <div className="mt-10">
+          <h1 className="text-green-500 font-bold font-mono ml-10 text-2xl">My Projects</h1>
+        </div>
+        <div className="mt-10 flex items-center">
+          <select className="mr-10 p-2 bg-neutral-800 text-green-500 border border-green-500 rounded-md font-mono cursor-pointer">
+            <option value="fineshed">Fineshed</option>
+            <option value="progress">In Progress</option>
+            <option value="paused">Paused</option>
+            <option value="think">Thinking</option>
+            <option value="canceled">Canceled</option>
+          </select>
+          <button className="bg-green-500 text-neutral-900 font-mono px-3 py-1 rounded-md hover:bg-green-600 transition-colors mr-10 cursor-pointer">
+            <img src='/images/assets/add.png' alt="Add" className="inline-block w-5 h-5 mr-2" />
+            New Project
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
