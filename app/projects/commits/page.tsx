9fc28314b9ce44a248481085623b68ea6f4f716d
Andre Nobre <afelipe1405@gmail.com>
2025-12-06 11:57:49 -0300

    Requisição GET sendo edetuada com sucesso

--- app/projects/commits/page.tsx
+++ app/projects/commits/page.tsx
@@ -2,13 +2,24 @@
 
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import Header from "../../../components/header";
+
+interface Project {
+  id: number;
+  name: string;
+  status: string;
+  repository: string;
+  location: string;
+}
 
 export default function Commits() {
   const searchParams = useSearchParams();
-  const projectId = searchParams.get("projectId"); // <= pega a ID da URL
-  
+  const projectId = searchParams.get("projectId");
 
+  const [project, setProject] = useState<Project | null>(null);
   const [commits, setCommits] = useState([]);
+  const [totalCommits, setTotalCommits] = useState(0);
 
   useEffect(() => {
     if (!projectId) return;
@@ -18,7 +29,7 @@ export default function Commits() {
         const token = localStorage.getItem("token");
 
         const response = await fetch(
-          `http://localhost:8080/projects/commits/${projectId}`, 
+          `http://localhost:8080/projects/commits/${projectId}`,
           {
             headers: {
               "Authorization": `Bearer ${token}`,
@@ -29,7 +40,11 @@ export default function Commits() {
         if (!response.ok) throw new Error("Erro ao carregar commits");
 
         const data = await response.json();
-        setCommits(data);
+
+        // Preenchendo cada parte corretamente
+        setProject(data.project);
+        setCommits(data.commits);
+        setTotalCommits(data.totalCommits);
 
       } catch (error) {
         console.error(error);
@@ -41,7 +56,31 @@ export default function Commits() {
 
   return (
     <div>
-      <h1>Commits do Projeto {projectId}</h1>
+      <Header/>
+      <div className="flex justify-between w-full mt-10">
+        <div className="flex items-center gap-4 ml-10">
+          <Link href="/projects/myprojects">
+            <img src="/images/assets/back.png" />
+          </Link>
+
+          <p className="text-green-500 font-mono text-xl">
+            [ {project ? project.name : "Carregando..."} ]
+          </p>
+          <Link href="/projects/myprojects">
+            <img className="w-5 h-5" src="/images/assets/edit.png" />
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4 mr-15">
+          <p className="text-green-500 font-mono text-xl">
+            Total de Commits: {totalCommits}
+          </p>
+
+          <button className="bg-green-500 text-neutral-900 font-mono px-3 py-1 rounded-md hover:bg-green-600 transition-colors cursor-pointer">
+            Commit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
