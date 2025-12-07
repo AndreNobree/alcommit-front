c525e20fea5048ae53dd964eacfbacae7c684935
Andre Nobre <afelipe1405@gmail.com>
2025-12-07 00:19:48 -0300

    criação de componentes na tela de commits + inteface

--- app/projects/commits/page.tsx
+++ app/projects/commits/page.tsx
@@ -21,6 +21,30 @@ export default function Commits() {
   const [commits, setCommits] = useState([]);
   const [totalCommits, setTotalCommits] = useState(0);
 
+  function parsePath(path: string) {
+  return path.replaceAll("\\", "/").split("/");
+}
+
+function FolderTree({ path }: { path: string }) {
+  const parts = parsePath(path);
+
+  return (
+    <div className="text-green-500 font-mono text-sm p-3">
+      {parts.map((folder, index) => (
+        <div key={index} className="flex items-center" style={{ marginLeft: index * 12 }}>
+          <img
+            src="/images/assets/folder.png"
+            className="w-4 h-4 mr-2"
+            alt="folder"
+          />
+          {folder}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
   useEffect(() => {
     if (!projectId) return;
 
@@ -63,8 +87,8 @@ export default function Commits() {
             <img src="/images/assets/back.png" />
           </Link>
 
-          <p className="text-green-500 font-mono text-xl">
-            [ {project ? project.name : "Carregando..."} ]
+          <p className="text-green-500 font-mono text-xl ml-10 mr-5">
+            [ {project ? project.name : "Loading..."} ]
           </p>
           <Link href="/projects/myprojects">
             <img className="w-5 h-5" src="/images/assets/edit.png" />
@@ -72,13 +96,52 @@ export default function Commits() {
         </div>
 
         <div className="flex items-center gap-4 mr-15">
-          <p className="text-green-500 font-mono text-xl">
-            Total de Commits: {totalCommits}
+          <p className="text-green-500 font-mono text-xl mr-10">
+            Total commits: {totalCommits}
           </p>
 
-          <button className="bg-green-500 text-neutral-900 font-mono px-3 py-1 rounded-md hover:bg-green-600 transition-colors cursor-pointer">
+          <button className="bg-green-500 text-neutral-900 font-mono px-5 py-2 rounded-md hover:bg-green-600 transition-colors cursor-pointer">
+            <img className="inline w-5 h-5 mr-2" src="/images/assets/commit.png" />
             Commit
           </button>
+          
+        </div>
+      </div>
+      <div className="mt-10 flex justify-between w-full">
+        <div className="ml-10 w-80 h-60 border border-green-500 flex flex-col justify-center align-baseline">
+          <p className="pl-3 pt-3 text-green-500 font-mono text-sm mb-2">
+            PATH:<br></br>
+            {project && <FolderTree path={project.location} />}
+          </p>
+        </div>
+        <div className="ml-10 mr-10 w-150 h-90 flex flex-col justify-center align-baseline">
+          <p className="text-green-500 font-mono text-xl mb-5 ">
+            <img className="inline w-5 h-5 mr-2" src="/images/assets/circle.png" />
+            {project ? project.status : "Loading..."}
+          </p>
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            Link Git:<br></br>
+            {project ? project.repository : "Loading..."}
+          </p>
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            PATH:<br></br>
+            {project ? project.location : "Loading..."}
+          </p>
+        </div>
+        <div className="ml-10 mr-10 w-150 h-90 flex flex-col justify-center align-baseline">
+          
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            DATA CREATE:<br></br>
+          </p>
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            USER CREATE:<br></br>
+          </p>
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            DATA UPDATE:<br></br>
+          </p>
+          <p className="text-green-500 font-mono text-xs mb-5 ">
+            USER UPDATE:<br></br>
+          </p>
         </div>
       </div>
     </div>
